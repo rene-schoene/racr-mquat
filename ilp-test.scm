8ee6c2c47e5650d2803b023ce180cae90cb0e995
rschoene <rene.schoene@tu-dresden.de>
2015-05-06 17:58:09 +0200

    Stub for testing correctness of ILP generation.

---
+++ ilp-test.scm
@@ -0,0 +1,10 @@
+#!r6rs
+
+(library
+ (mquat ilp-test)
+ (export)
+ (import (rnrs) (racr core) (racr testing)
+         (mquat constants))
+ ;; Testing generated ILP, whether correct results are computed for small models
+ 
+ )
