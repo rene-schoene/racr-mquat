b9569fb073840e66e64b3e97871e74c7629a853e
rschoene <rene.schoene@tu-dresden.de>
2015-05-07 12:55:58 +0200

    Enable single-file compiling.

--- install.sh
+++ install.sh
@@ -4,8 +4,15 @@ function compile() {
 	plt-r6rs ++path $racrbin --install --collections racket-bin $1
 }
 
-rm -rf racket-bin
+#rm -rf racket-bin
 
 cat dependencies.txt | while read line; do
-	compile "$line.scm"
+	input="$line.scm"
+	if [ -n "$1" ] && [[ ! "$1" == $input ]]; then continue; fi
+	if [[ "$input" == "main.scm" ]]
+	then output="racket-bin/mquat/main_.ss"
+	else output="racket-bin/mquat/$line.ss"
+	fi
+	rm -f $output
+	compile $input
 done
