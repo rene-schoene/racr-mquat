0442f8c3e22c958031671f3f31f1da2575054eee
rschoene <rene.schoene@tu-dresden.de>
2015-05-20 16:01:39 +0200

    Small measurement enhancements.
    - specs and ast-generation-time saved for each run

--- ilp-measurement.scm
+++ ilp-measurement.scm
@@ -2,20 +2,31 @@
 
 (library
  (mquat ilp-measurement)
- (export run-tests)
+ (export)
  (import (rnrs) (racr core) (srfi :19)
          (mquat ast) (mquat basic-ag) (mquat utils) (mquat main) (mquat ilp) (mquat ast-generation) (mquat ui))
  
- (define params
-   (list (list 1 "001" 10 0 1 1 2)
-         (list 2 "002" 10 0 10 1 2)
-         (list 3 "003" 10 0 10 10 2)
-         (list 4 "004" 10 0 10 10 10)
-         (list 5 "005" 20 0 1 1 2)
-         (list 6 "006" 20 0 10 1 2)
-         (list 7 "007" 20 0 10 10 2)
-         (list 8 "008" 20 0 10 10 10)))
+ (define (dirname id)
+   (let ([id-s (number->string id)])
+     (string-append (make-string (- 3 (string-length id-s)) #\0) id-s)))
  
+ (define params
+   (map (lambda (l) (append (cons (dirname (car l)) (cdr l))
+                            (list (list (lambda _ #t) #f #f #f))))
+        (list (list  1 10 0 1 1 2)
+              (list  2 10 0 10 1 2)
+              (list  3 10 0 10 10 2)
+              (list  4 10 0 10 10 10)
+              (list  5 20 0 1 1 2)
+              (list  6 20 0 10 1 2)
+              (list  7 20 0 10 10 2)
+              (list  8 20 0 10 10 10)
+              (list  9 20 0 20 2 2)
+              (list 10 25 0 25 2 2)
+              (list 11 30 0 30 2 2)
+              (list 12 40 0 40 2 2)
+              (list 13 50 0 50 2 2))))
+   
  (define (rw comp-name restype prop-name new-value ast)
    (debug comp-name prop-name new-value ast)
    (rewrite-terminal 'value (=provided-clause (find (lambda (pe) (eq? (->name pe) comp-name))
@@ -32,14 +43,16 @@
    (let* ([name (string-append "profiling/" id-s "/" suffix)]
           [result (time-it (lambda _ (save-ilp (string-append name ".lp") ast)))])
      (save-to-file (string-append name ".lp.time") (list (time-second (cdr result)) (time-nanosecond (cdr result))))))
- 
- ;;; Test definitions
- 
- (define (run-tests . ids) (for-each run-test ids))
 
- (define (run-simple-test id-s specs)
-   (let* ([all-specs (append specs (list (list (lambda _ #t) #f #f #f)))]
-          [ast (apply create-system all-specs)]
+ 
+ (define (cst id-s specs) ; [c]reate-[s]ystem-[t]imed
+   (let* ([name (string-append "profiling/" id-s "/specs")]
+          [result (time-it (lambda _ (apply create-system specs)))])
+     (save-to-file name (cons* (time-second (cdr result)) (time-nanosecond (cdr result)) specs))
+     (car result)))
+ 
+ (define (run-test id-s specs)
+   (let* ([ast (cst id-s specs)]
           [rt (ast-child 1 (->ResourceType* (->HWRoot ast)))])
      (sit id-s "01-init" ast)
      (rw 'res-1 rt 'load 0.1 ast) (sit id-s "02-comp1" ast)
@@ -51,18 +64,18 @@
      (rw* rt 'load #f ast) (sit id-s "07-every-comp-rand" ast)
      (rw* rt 'load #f ast) (sit id-s "08-every-comp-rand" ast)))
 
- (define (run-test id)
-   (display ".")
-   (let ([entry (assq id params)])
-     (if entry (run-simple-test (cadr entry) (cddr entry))
-         (error 'run-test "Invalid id" id))))
-
- (when (find (lambda (arg) (string=? "all" arg)) (command-line))
-   (let ([was-debugging? debugging?])
-     (set!debugging #f)
-     (run-tests 1 2 3 4 5)
-     (set!debugging was-debugging?)))
-
- (when (find (lambda (arg) (string=? "dirs" arg)) (command-line))
-   (set!debugging #f)
-   (for-each (lambda (entry) (display (cadr entry)) (display " ")) params)))
+ (cond
+   [(find (lambda (arg) (string=? "all" arg)) (command-line))
+    (let ([was-debugging? debugging?])
+      (set!debugging #f)
+      (for-each run-test (map car params) (map cdr params))
+      (set!debugging was-debugging?))]
+   [(find (lambda (arg) (string=? "dirs" arg)) (command-line))
+    (set!debugging #f)
+    (for-each (lambda (entry) (display (car entry)) (display " ")) params)]
+   [(> (length (command-line)) 1)
+    (let ([ids (map dirname (cdr command-line))]
+          [was-debugging? debugging?])
+      (set!debugging #f)
+      (for-each run-test (ids) (map (lambda (id) (assq id params)) ids))
+      (set!debugging was-debugging?))]))
--- ilp_measurement.py
+++ ilp_measurement.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, re, os, csv
+import sys, re, os, csv, timeit
 from fabric.colors import red
 from fabric.api import task, local, lcd
 from constants import RACR_BIN, MQUAT_BIN
@@ -17,7 +17,7 @@ def measure_racket():
 	local('racket -S %s -S %s ilp-measurement.scm all' % (RACR_BIN, MQUAT_BIN))
 
 results = 'results.csv'
-header = 'step, ilpgen, ilpsol'
+header = ['step', 'ilp-gen', 'rows', 'cols', 'non-zero', 'ilp-sol', 't-ilp-sol']
 
 @task
 def measure_glpsol(*dirs):
@@ -35,21 +35,31 @@ def measure_glpsol(*dirs):
 		os.chdir(d)
 		with open(results, 'w') as fd:
 			writer = csv.writer(fd)
+			writer.writerow(header)
 			files = os.listdir('.')
 			files.sort()
 			for ilp in files:
 				if not ilp.endswith('.lp'):
 					continue
+				start = timeit.default_timer()
 				out = local('glpsol --lp %s -w %s' % (ilp, ilp.replace('lp','sol')), capture = True)
+				stop = timeit.default_timer()
 				if re.search('INTEGER OPTIMAL SOLUTION FOUND', out):
 					duration = re.search('Time used:[\s]*(.*?) secs', out).group(1)
-					print 'Solution found for %s in %s secs' % (ilp, duration)
+					rows, cols, nonzeros = re.search('(\d+) rows, (\d+) columns, (\d+) non-zeros', out).groups()
+					print 'Solution found for %s in %s secs (timeit: %s secs)' % (ilp, duration, stop - start)
 				else:
 					print red('No solution found for %s' % ilp)
 					duration = -1
 				with open(ilp+'.time') as tfd:
-					s,ns = tfd.readline().split()
-				writer.writerow([ilp.rsplit('.', 1)[0], '.'.join([s,ns]), duration])
+					ilp_gen_time = '.'.join(tfd.readline().split())
+				writer.writerow([ilp.rsplit('.', 1)[0], ilp_gen_time, rows, cols, nonzeros, duration, stop-start])
+	os.chdir(old_cd)
+	conflate_results()
+
+@task
+def conflate_results():
+	local('tail -n +1 profiling/*/results.csv > profiling/all-results')
 
 @task
 def t(*dirs):
